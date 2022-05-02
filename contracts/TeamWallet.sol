8d65e099d891a8659a35ab32db42c44335613c1e
Amaechi Okolobi <okolobiam@gmail.com>
2022-05-02 18:16:53 +0100

    Locked Wallets

---
+++ contracts/TeamWallet.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TeamWallet is Ownable{
+
+    uint public unlockDate;
+    uint256 public createdAt;
+
+    event Received(address from, uint256 amount);
+    event WithdrawTokens(address tokenContract, address to, uint256 amount);
+
+    constructor() {
+        unlockDate = 365 days;
+        createdAt = block.timestamp;
+    }
+
+    // callable by owner only, after specified time, only for Tokens implementing ERC20
+    function withdrawTokens(address _tokenContract) onlyOwner public {
+       require(block.timestamp >= unlockDate);
+       ERC20 token = ERC20(_tokenContract);
+       //now send all the token balance
+       uint256 tokenBalance = token.balanceOf(address(this));
+       token.transfer(_msgSender(), tokenBalance);
+       emit WithdrawTokens(_tokenContract, msg.sender, tokenBalance);
+    }
+
+    function info() public view returns(uint256, uint256, uint256) {
+        return (unlockDate, createdAt, address(this).balance);
+    }
+
+}
